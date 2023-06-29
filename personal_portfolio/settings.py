527074499d532613f62a8bfe550ff687f0781871
Toru Hironaka <toruhironaka@hotmail.com>
2023-06-29 11:19:48 +0000

    added .gitignore on 06/28/23

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wg47wzl54v==qw@fqtswnhtd4umwgk*y%!0&8(p_%jxyr+m6wb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thironaka.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
